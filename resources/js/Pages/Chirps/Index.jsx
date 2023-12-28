d9931480cf77bf5d8b1bc43adb34f39dac4d1d49
suryabk <surya27bk@gmail.com>
2023-12-29 01:12:49 +0700

    show chat data from specific user

--- resources/js/Pages/Chirps/Index.jsx
+++ resources/js/Pages/Chirps/Index.jsx
@@ -1,17 +1,18 @@
-import React from 'react';
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
-import InputError from '@/Components/InputError';
-import PrimaryButton from '@/Components/PrimaryButton';
-import { useForm, Head } from '@inertiajs/react';
+import React from "react";
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
+import InputError from "@/Components/InputError";
+import PrimaryButton from "@/Components/PrimaryButton";
+import { useForm, Head } from "@inertiajs/react";
+import Chirp from "@/Components/Chirp";
 
-export default function Index({ auth }) {
+export default function Index({ auth, chirps }) {
     const { data, setData, post, processing, reset, errors } = useForm({
-        message: '',
+        message: "",
     });
 
     const submit = (e) => {
         e.preventDefault();
-        post(route('chirps.store'), { onSuccess: () => reset() });
+        post(route("chirps.store"), { onSuccess: () => reset() });
     };
 
     return (
@@ -24,11 +25,18 @@ export default function Index({ auth }) {
                         value={data.message}
                         placeholder="What's on your mind?"
                         className="block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"
-                        onChange={e => setData('message', e.target.value)}
+                        onChange={(e) => setData("message", e.target.value)}
                     ></textarea>
                     <InputError message={errors.message} className="mt-2" />
-                    <PrimaryButton className="mt-4" disabled={processing}>Chirp</PrimaryButton>
+                    <PrimaryButton className="mt-4" disabled={processing}>
+                        Chirp
+                    </PrimaryButton>
                 </form>
+                <div className="mt-6 bg-white shadow-sm rounded-lg divide-y">
+                    {chirps.map((chirp) => (
+                        <Chirp key={chirp.id} chirp={chirp} />
+                    ))}
+                </div>
             </div>
         </AuthenticatedLayout>
     );
